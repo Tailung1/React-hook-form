6f324f5c5f6ac213eb57061b1d865c955e25f44b
Tailung1 <ramazsamitashvili11@gmail.com>
2025-03-04 03:51:23 +0400

    add yup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 function App() {
   interface Inputs {
@@ -7,55 +9,45 @@ function App() {
     lastName: string;
     Email: string;
   }
+
+  const schema = yup.object({
+    name: yup.string().required("Name must not be empty"),
+
+    lastName: yup
+      .string()
+      .required("LastName must not be empty")
+      .min(5, "Name must not be more than 5 charachters"),
+
+    Email: yup.string().email().required("Email must not be empty"),
+  });
+
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<Inputs>();
+  } = useForm<Inputs>({ resolver: yupResolver(schema) });
+
   const onSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log(data);
+    console.log(data); // consols only when all input are valid
   };
-  console.log("errors", errors);
+
+  //   const name = watch("name"); // colsols only name property
+  //   const wholeObj = watch(); // consols whole obj dynamically
+  //   console.log(name);
 
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          type="text"
-          {...register("name", {
-            required: "Name must not be empty",
-            maxLength: {
-              value: 10,
-              message: "Name must not be more than 10 charachters",
-            },
-          })}
-        />
+        <input type="text" {...register("name")} />
         {errors.name && <p>{errors.name.message}</p>}
 
-        <input
-          type="text "
-          {...register("lastName", {
-            required: "lastName must not be empty",
-            minLength: {
-              value: 5,
-              message: "lastName must contain at leasr 5 charachters",
-            },
-          })}
-        />
+        <input type="text " {...register("lastName")} />
         {errors.lastName && <p>{errors.lastName.message}</p>}
 
-        <input
-          type="text "
-          {...register("Email", {
-            required: "Email must not be empty",
-            pattern: {
-              value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-              message: "Email isn't valid",
-            },
-          })}
-        />
+        <input type="text " {...register("Email")} />
         {errors.Email && <p>{errors.Email.message}</p>}
+
         <button type="submit">Submit</button>
       </form>
     </>
